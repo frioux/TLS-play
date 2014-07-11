2a411bf1488f7217c0db46bbe17fd9c00ad0451f
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-07-11 15:33:07 -0500

    basic IO::Async::SSL server

---
+++ server.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+
+use 5.20.0;
+use warnings;
+
+use experimental 'signatures';
+
+use IO::Async::Loop;
+use IO::Async::SSL;
+
+$IO::Socket::SSL::DEBUG = 3;
+
+my $loop = IO::Async::Loop->new;
+
+my $server = $loop->SSL_listen(
+   addr => {
+      family   => 'inet',
+      socktype => 'stream',
+      port     => 9932,
+      ip       => '127.0.0.1',
+   },
+
+   SSL_key_file  => 'key.pem',
+   SSL_cert_file => 'cert.pem',
+
+   on_stream => sub {
+      my ( $stream ) = @_;
+
+      $stream->configure(
+         on_read => sub ($self, $buffref, $eof) {
+            $stream->write($$buffref);
+            $$buffref = '';
+            0
+         },
+      );
+
+      $loop->add( $stream );
+   },
+
+   on_ssl_error     => sub { print STDERR "Cannot negotiate SSL - $_[-1]\n"; },
+   on_resolve_error => sub { die "Cannot resolve - $_[1]\n"; },
+   on_listen_error  => sub { die "Cannot listen - $_[1]\n"; },
+);
+
+$loop->run;
