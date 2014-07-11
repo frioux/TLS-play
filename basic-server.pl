ab132dcdf774a241c250322b58d7311f63c77d58
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-07-11 15:32:32 -0500

    basic IO::Socket::SSL server

---
+++ basic-server.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use 5.20.0;
+use warnings;
+
+use IO::Socket::SSL;
+$IO::Socket::SSL::DEBUG = 3;
+
+# simple server
+my $server = IO::Socket::SSL->new(
+    # where to listen
+    LocalAddr => '127.0.0.1',
+    LocalPort => 9934,
+    Listen => 10,
+
+    # which certificate to offer
+    # with SNI support there can be different certificates per hostname
+    SSL_cert_file => './cert.pem',
+    SSL_key_file => './key.pem',
+) or die "failed to listen: $!";
+
+# accept client
+my $client = $server->accept or die
+    "failed to accept or ssl handshake: $!,$SSL_ERROR";
